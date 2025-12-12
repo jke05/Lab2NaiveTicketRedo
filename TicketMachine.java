a590e845e8f15804807db5212659e117ebeea17b
John <jkespinosa.25@yahoo.com>
2025-12-11 22:30:44 -0500

    answered remaining questions

--- TicketMachine.java
+++ TicketMachine.java
@@ -112,4 +112,8 @@ public class TicketMachine
     public void showPrice(){
         System.out.println("The price of a ticket is " + price + " cents."); 
     }
+    
+    public void empty(){
+        total = 0;
+    }
 }
